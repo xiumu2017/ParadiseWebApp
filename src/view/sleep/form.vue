86f85069992ec045b86a69eb45f56fe2ecb1c8c9
Paradise <zdparadise@icloud.com>
2021-04-06 19:19:19 +0800

    完善睡眠页面

---
+++ src/view/sleep/form.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="sleep-form">
+    <van-form @submit="onSubmit">
+      <van-field
+        readonly
+        clickable
+        name="date"
+        :value="formData.date"
+        label="日期"
+        placeholder="点击选择日期"
+        @click="showCalendar = true"
+      />
+      <van-calendar
+        v-model="showCalendar"
+        :min-date="minDate"
+        @confirm="onConfirm4Date"
+      />
+      <van-field
+        readonly
+        clickable
+        name="type"
+        :value="type"
+        label="类型"
+        placeholder="点击选择类型"
+        @click="showPicker = true"
+      />
+      <van-popup v-model="showPicker" position="bottom">
+        <van-picker
+          show-toolbar
+          :columns="typeArr"
+          @confirm="onConfirm4Type"
+          @cancel="showPicker = false"
+        />
+      </van-popup>
+      <van-field
+        v-model="formData.what"
+        name="what"
+        label="吃什么"
+        placeholder=""
+        :rules="[{ required: true, message: '请填写吃什么' }]"
+      />
+      <van-field
+        v-model="formData.place"
+        name="place"
+        label="在哪儿吃"
+        placeholder=""
+        :rules="[{ required: true, message: '请填写在哪儿吃' }]"
+      />
+      <van-field
+        v-model="formData.cost"
+        type="number"
+        name="cost"
+        label="花了多少"
+        placeholder=""
+        :rules="[{ required: true, message: '请填写花了多少' }]"
+      />
+      <van-field
+        readonly
+        clickable
+        name="payType"
+        :value="payType"
+        label="支付方式"
+        placeholder="点击选择支付方式"
+        @click="showPayTypePicker = true"
+      />
+      <van-popup v-model="showPayTypePicker" position="bottom">
+        <van-picker
+          show-toolbar
+          :columns="payTypeArr"
+          @confirm="onConfirm4PayType"
+          @cancel="showPayTypePicker = false"
+        />
+      </van-popup>
+      <van-field
+        v-model="formData.remark"
+        rows="3"
+        autosize
+        label="备注"
+        name="remark"
+        type="textarea"
+        placeholder="请输入备注"
+      />
+      <div style="margin: 16px">
+        <van-button round block type="info" native-type="submit"
+          >提交</van-button
+        >
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+import { create, getPayTypes, getTypes } from "@/api/meal.js";
+
+export default {
+  data() {
+    return {
+      minDate: new Date(2021, 0, 1),
+      maxDate: new Date(2021, 5, 1),
+      formData: {
+        what: "",
+        place: "",
+        cost: "",
+        date: "",
+        type: "",
+        payType: "",
+        remark: "",
+      },
+      date: "",
+      type: "",
+      payType: "",
+      showCalendar: false,
+      showPicker: false,
+      showPayTypePicker: false,
+      typeArr: [],
+      payTypeArr: [],
+    };
+  },
+  created() {
+    getPayTypes().then((res) => {
+      this.payTypeArr = res.data;
+    });
+    getTypes().then((res) => {
+      this.typeArr = res.data;
+    });
+  },
+  methods: {
+    onSubmit() {
+      const data = Object.assign({}, this.formData);
+      data.date = data.date + " 00:00:00";
+      data.cost = data.cost * 100;
+      console.log("提交用餐数据：", data);
+      create(data).then((res) => {
+        if (res.code === 200) {
+          this.$toast.success(res.message);
+          this.$router.push('meal');
+        } else {
+          this.$toast.fail(res.message);
+        }
+      });
+    },
+    onConfirm4Date(date) {
+      this.formData.date = `${date.getFullYear()}-${
+        date.getMonth() + 1
+      }-${date.getDate()}`;
+      this.showCalendar = false;
+    },
+    onConfirm4Type(value, index) {
+      this.formData.type = index;
+      this.type = value;
+      this.showPicker = false;
+    },
+    onConfirm4PayType(value, index) {
+      this.formData.payType = index;
+      this.payType = value;
+      this.showPayTypePicker = false;
+    },
+  },
+};
+</script>
--- src/view/sleep/index.vue
+++ src/view/sleep/index.vue
@@ -3,9 +3,7 @@
     <van-divider> {{ now }} </van-divider>
 
     <!-- // 按钮组，上床了，我醒了，我起了 -->
-    <!-- 展示最近的睡眠记录？ -->
-
-    <van-row style="margin: 10px">
+    <van-row style="margin: 10px" v-show="showSleepBtn">
       <van-button
         icon="https://paradise-1256237186.cos.ap-nanjing.myqcloud.com/1705357B%E7%9D%A1%E8%A7%89.png"
         color="linear-gradient(to right, #fff, #004)"
@@ -15,7 +13,7 @@
         上床睡觉咯
       </van-button>
     </van-row>
-    <van-row style="margin: 10px">
+    <van-row style="margin: 10px" v-show="showWakeBtn">
       <van-button
         icon="https://paradise-1256237186.cos.ap-nanjing.myqcloud.com/170806G3%E5%A4%AA%E9%98%B3-.png"
         color="linear-gradient(to left, #fff, #004)"
@@ -25,7 +23,7 @@
         我醒啦
       </van-button>
     </van-row>
-    <van-row style="margin: 10px">
+    <van-row style="margin: 10px" v-show="showUpBtn">
       <van-button
         icon="https://paradise-1256237186.cos.ap-nanjing.myqcloud.com/170802SH%E5%8A%A0%E6%B2%B9.png"
         color="linear-gradient(to right, #fff, #004)"
@@ -35,28 +33,87 @@
         起床咯
       </van-button>
     </van-row>
+    <!-- 展示最近的睡眠记录？ -->
+    <van-row style="margin: 10px">
+      <van-cell-group
+        v-for="(item, index) in sleepData"
+        :key="index"
+        :title="item.date + ' ' + item.title"
+      >
+        <van-cell :title="item.title">
+          <template #label>
+            <span class="custom-title" style="font-size: 15px">{{
+              item.st + " ~ " + item.et
+            }}</span
+            ><br />
+            <van-rate
+              v-model="item.sleepQuality"
+              :size="15"
+              color="#ffd21e"
+              void-icon="star"
+              void-color="#eee"
+            /><br />
+            <span class="custom-title" v-show="item.memory !== '/'"
+              >睡前回忆：{{ item.memory }}</span
+            >
+            <br />
+            <span class="custom-title" v-show="item.remark !== '/'"
+              >备注：{{ item.remark }}</span
+            >
+            <br />
+            <van-tag type="danger" v-show="item.lateReason !== '/'"
+              >熬夜原因：{{ item.lateReason }}</van-tag
+            >
+            <br />
+            <van-tag type="success" v-if="item.bestTime !== '/'">{{
+              item.bestTime
+            }}</van-tag>
+          </template>
+        </van-cell>
+      </van-cell-group>
+    </van-row>
   </div>
 </template>
 
 <script>
-import { sign } from "@/api/sleep.js";
+import { sign, fetch } from "@/api/sleep.js";
 import { Dialog } from "vant";
+import Vue from "vue";
+import { Rate } from "vant";
 
+Vue.use(Rate);
 let interval;
 export default {
   name: "sleep",
   data() {
     return {
       now: "大漠孤烟直，长河落日圆",
-      weeks: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "SUN"],
+      weeks: ["Sun🔆", "Mon💥", "Tue💪", "Wed🙃", "Thu🤬", "Fri👏", "Sat🎉"],
       beforeCloseFun: null,
       val: 0,
+      sleepData: [],
+      showSleepBtn: false,
+      showWakeBtn: false,
+      showUpBtn: false,
     };
   },
   created() {
     interval = setInterval(() => {
       this.now = this.formatTime();
     }, 1000);
+    this.fetchData();
+    // 如果是睡觉时间，显示1隐藏23
+    let hour = new Date().getHours();
+    if (hour > 18) {
+      this.showSleepBtn = true;
+      this.showWakeBtn = false;
+      this.showUpBtn = false;
+    }
+    if (hour > 4 && hour <= 12) {
+      this.showSleepBtn = false;
+      this.showWakeBtn = true;
+      this.showUpBtn = true;
+    }
   },
   methods: {
     formatTime() {
@@ -68,8 +125,21 @@ export default {
           ${now.getHours() < 10 ? "0" + now.getHours() : now.getHours()}:
           ${now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes()}:
           ${now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds()} 
-          ` + this.weeks[now.getDay() - 1]
+          ` + this.weeks[now.getDay()]
       );
+    },
+    fetchData() {
+      fetch({ pageNum: 1, pageSize: 7 }).then((res) => {
+        if (res.code === 200) {
+          this.sleepData = res.data.list;
+          this.sleepData.forEach((item) => {
+            item.title = this.weeks[new Date(item.date).getDay()];
+            item.date = this.formatDate(item.date);
+            item.st = this.formatDateTime(item.sleepTime);
+            item.et = this.formatDateTime(item.upTime);
+          });
+        }
+      });
     },
     sign(val) {
       console.log("val", val);
@@ -82,6 +152,7 @@ export default {
             .then((res) => {
               if (res.code === 200) {
                 this.$toast.success(res.message);
+                this.fetchData();
               } else {
                 this.$toast.fail(res.message);
               }
@@ -96,10 +167,19 @@ export default {
       };
       // 提交之前 二次确认
       Dialog.confirm({
-        title: "确认",
-        message: "弹窗内容",
+        title: "确认提交",
+        message: "沧海月明珠有泪，蓝田日暖玉生烟。",
         beforeClose,
       });
+    },
+    formatDate(date) {
+      return date.split(" ")[0];
+    },
+    formatDateTime(date) {
+      if (!date) {
+        return "";
+      }
+      return date.split(" ")[1].replace(":00", "");
     },
   },
   beforeDestroy() {
