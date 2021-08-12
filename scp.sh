9653b2418ca5abebb4389d62a8f4a44adc18893a
Paradise <dz.paradis3@gmail.com>
2021-08-12 20:18:28 +0800

    scp fun

---
+++ scp.sh
@@ -0,0 +1,1 @@
+scp -P 20202 -r .\dist\ root@121.36.215.87:/data/docker/frontend/www/app
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,13 +3,15 @@ import Cookies from 'js-cookie'
 const TokenKey = 'vue_admin_template_token'
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+    return Cookies.get(TokenKey)
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+    let num = 7; //失效时间7天
+    let time = new Date(new Date().getTime() + num * 24 * 60 * 60 * 1000);
+    return Cookies.set(TokenKey, token, {expires: time})
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+    return Cookies.remove(TokenKey)
 }
